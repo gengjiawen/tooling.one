bfa853740c578182a0db964043362d5242c7e91d
gengjiawen <technicalcute@gmail.com>
2026-06-06 11:50:58 +0000

    Move sponsor section to bottom of homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,19 +6,6 @@ export default function HomePage() {
   return (
     <Layout>
       <div className="p-5 max-w-(--breakpoint-lg)">
-        <a
-          target="_blank"
-          rel="noopener nofollow"
-          href="https://github.com/sponsors/egoist"
-          className="mb-5 border-2 border-pink-400 text-pink-500 font-bold flex items-center justify-center p-5 rounded-lg text-xl space-x-2 hover:bg-pink-50"
-        >
-            <img
-            alt="github sponsors logo"
-            src="/github-sponsors-logo.svg"
-            className="w-1- h-10"
-          />
-          <span>Sponsor this project on GitHub.</span>
-        </a>
         <div className="bg-cyan-100 text-cyan-700 font-bold p-5 rounded-lg space-y-3">
           <p>
             tooling.one is a collection of useful tools for developers, it works
@@ -52,6 +39,21 @@ export default function HomePage() {
             </Link>
           )
         })}
+      </div>
+      <div className="p-5 max-w-(--breakpoint-lg)">
+        <a
+          target="_blank"
+          rel="noopener nofollow"
+          href="https://github.com/sponsors/egoist"
+          className="border-2 border-pink-400 text-pink-500 font-bold flex items-center justify-center p-5 rounded-lg text-xl space-x-2 hover:bg-pink-50"
+        >
+          <img
+            alt="github sponsors logo"
+            src="/github-sponsors-logo.svg"
+            className="w-10 h-10"
+          />
+          <span>Sponsor this project on GitHub.</span>
+        </a>
       </div>
     </Layout>
   )
